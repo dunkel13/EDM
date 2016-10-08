b1393703016ad8422350a92753ecc6c536eaf83d
dunkel13 <fedom9@gmail.com>
2016-10-08 17:56:12 -0500

    Update Tabla_contingencia.R

--- Tabla_contingencia.R
+++ Tabla_contingencia.R
@@ -5,7 +5,7 @@ CCLatin <- read.csv2("Muestra_CC.csv", sep = ";")
 # lee el archivo Muestra_CC, que debe estar guardado en el directorio de trabajo
 CCLatin [CCLatin==""]<-NA
 CiuXNed<-table(CCLatin$city, CCLatin$p7)
-# Crea la tabla CiuXNed con las columnas city y p7
+# Crea la tabla de contingencias CiuXNed con las columnas city y p7
 Ne<-c("Sined","Prim","Sec","Tec","Uni", "Pos")
 stad<-c("B Hor","Bog","Car","LPaz","Med","Mex","Mont","Quit")
 # Crea los vectores Ne y Stad para asignar los nombres a las columnas y filas respectivamente
@@ -13,7 +13,7 @@ colnames(CiuXNed)<-Ne
 rownames(CiuXNed)<-stad
 # Asigna los nombres a las columnas y filas de CiuXNed
 CC<-xtable(CiuXNed, Caption = "Nivel educativo de encuestados en la ECC por ciudades")
-# Crea la tabla de contingencia CC
+# Asigna a CC la tabla de contingencias, que pueda ser impresa como una tabla LaTeX
 print(CC)
 v<-c(rep(0,6))
 # Crea un vector de ceros
